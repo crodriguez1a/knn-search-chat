66b62d74d44043c762755d9721623015a75c694c
crodriguez1a <crodriguez1a@gmail.com>
2019-11-15 06:49:01 -0500

    Initial commit

---
+++ app/app.py
@@ -0,0 +1,72 @@
+import tensorflow as tf
+import tensorflow_hub as hub
+import numpy as np
+import os
+import nmslib
+
+USENC_2 = "https://tfhub.dev/google/universal-sentence-encoder/2"
+# TODO Use LITE encoder
+
+def load_encoder(module_url:str) -> hub.module.Module:
+    # @param ["https://tfhub.dev/google/universal-sentence-encoder/2", "https://tfhub.dev/google/universal-sentence-encoder-large/3"]
+    # import as tf module
+    return hub.Module(module_url)
+
+def encode(embed: hub.module.Module, messages: list) -> np.ndarray:
+    # initialize tf session graph
+    with tf.Session() as session:
+        session.run([ tf.global_variables_initializer(),
+                      tf.tables_initializer()
+                    ])
+        return session.run(embed(messages))
+
+def create_index(embeddings: np.ndarray, method: str='hnsw') -> nmslib.dist.FloatIndex:
+    """
+    Ref: https://github.com/nmslib/nmslib/blob/master/manual/methods.md
+    """
+    # initialize a new index, using a HNSW index on Cosine Similarity
+    search_index: nmslib.dist.FloatIndex = nmslib.init(method=method, space='cosinesimil')
+    search_index.addDataPointBatch(embeddings)
+    search_index.createIndex({'post': 2}, print_progress=True)
+
+    return search_index
+
+def search(query_vector: np.ndarray, n_results:int = 3) -> tuple:
+    idx, dist = search_index.knnQuery(query_vector, k=n_results)
+    return (idx, dist)
+
+if __name__ == "__main__":
+    # sample bot
+    qna: dict = {
+        'queries' : {
+            "favorite baseball team" : "fav_baseball",
+            "best baseball team" : "fav_baseball"
+          },
+
+          'answers' : {
+              'fav_baseball': ["NY Yankees, obviously", "have to say...Yankees"]
+          }
+    }
+
+    # load encoder module
+    print("Loading encoder...")
+    embed: hub.module.Module = load_encoder(USENC_2)
+
+    # assemble possible queries
+    queries: list = list(qna['queries'].keys())
+
+    # encode queries
+    print('Encoding bot data...')
+    query_embeddings: np.ndarray = encode(embed, queries)
+
+    # create search index
+    search_index: nmslib.dist.FloatIndex = create_index(query_embeddings)
+
+    # TEMP
+    idx, dist = search(encode(embed, ["What's your favorite baseball team"]))
+
+    if idx.any():
+        search_result: str = queries[idx[0]]
+        answer_key: str = qna['queries'][search_result]
+        answer: str = qna['answers'][answer_key]
+        print(random.choice(answer))
---
+++ tests/test_app.py
@@ -0,0 +1,5 @@
+
+
+class TestApp:
+    def test_can_answer_basic(self):
+        assert True == False
